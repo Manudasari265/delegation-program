1f74c7147169bc78d8834b0b093b6c84af921cf0
Gabriele Picco <piccogabriele@gmail.com>
2025-09-05 16:33:52 +0200

    feat: enforce identity and add broadcast consts (#85)
    * feat: enforce identity and add broadcast consts
    
    * chore: add documentation

--- src/consts.rs
+++ src/consts.rs
@@ -20,3 +20,8 @@ pub const DELEGATION_PROGRAM_ID: Pubkey = crate::id();
 /// Default validator identity (used when none is provided during delegation).
 pub const DEFAULT_VALIDATOR_IDENTITY: Pubkey =
     pubkey!("MAS1Dt9qreoRMQ14YQuhg8UTZMMzDdKhmkZMECCzk57");
+
+/// The broadcast identity marks an account as undelegatable.
+/// Validators treat it as always delegatable, which is safe since such accounts
+/// cannot be committed or delegated
+pub const BROADCAST_IDENTITY: Pubkey = pubkey!("Broadcast1111111111111111111111111111111111");
